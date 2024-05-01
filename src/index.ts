d69b2998d11414a93d42955cbca84efab628ae2d
Robot-Inventor <30.keita@gmail.com>
2024-05-01 23:51:10 +0900

    fix: change import assertion to import attribute

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import grammar from "../syntaxes/regex.tmLanguage.json" assert { type: "json" };
+import grammar from "../syntaxes/regex.tmLanguage.json" with { type: "json" };
 import type { LanguageRegistration } from "shiki";
 
 export default grammar as unknown as LanguageRegistration;
